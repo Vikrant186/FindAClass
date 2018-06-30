d1bddb7e9e174dbdc65b195d1d0465d92354a9ee
vikrant186 <vikrantkamble186@gmail.com>
2018-06-30 23:21:17 +0530

    initial commit

---
+++ GitFirstApp/GitFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sName = "Vikrant";
+            Console.WriteLine(sName);
+            Console.ReadLine();
+        }
+    }
+}
